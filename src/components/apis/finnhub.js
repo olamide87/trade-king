2f1cd5b42d7eb1652fbf36f2207e4ded8a367879
Olamide Oladinni <olajide1987@gmail.com>
2022-12-20 22:46:11 -0600

    refactored watch list data

--- src/components/apis/finnhub.js
+++ src/components/apis/finnhub.js
@@ -1,4 +1,9 @@
 import axios from "axios"
 
-export default axios.create({ baseURL: 
-    "https://finnhub.io/api/v1" })
+const TOKEN = "ceduol2ad3i32ebrsikgceduol2ad3i32ebrsil0"
+
+export default axios.create({ 
+    baseURL: "https://finnhub.io/api/v1",
+    params: {
+    token: TOKEN
+} })
